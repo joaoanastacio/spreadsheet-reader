7eda6f47a234322ff7c94f4fcf9e63e534f8f99f
Joao Victor Anastacio <joaoanastacio@Joaos-MacBook-Pro.local>
2024-02-07 21:29:14 -0300

    init: include initial files to the project

---
+++ src/main/java/com/mypoc/spreadsheetreader/SpreadsheetReaderApplication.java
@@ -0,0 +1,13 @@
+package com.mypoc.spreadsheetreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpreadsheetReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpreadsheetReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mypoc/spreadsheetreader/SpreadsheetReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypoc.spreadsheetreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpreadsheetReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
